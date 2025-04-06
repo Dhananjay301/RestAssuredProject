187584c55cc554d99c3ce3bb634681accf33385d
Dhananjay301 <dhananjaypatil301@gmail.com>
2025-04-06 13:51:21 +0530

    ceate and get user details

--- src/test/java/day1/HttpsMethos.java
+++ src/test/java/day1/HttpsMethos.java
@@ -6,21 +6,43 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+
 public class HttpsMethos {
+
+//	@Test
+//	void getUser() {
+//
+//		     given()
+//
+//			.when()
+//			   .get("https://reqres.in/api/users?page=2")
+//
+//			.then()
+//			   .statusCode(200)
+//			   .body("page", equalTo(2))
+//			   .log().all();
+//
+//	}
 	
 	@Test
-	void user() {
+	void createUser() {
+		HashMap mp=new HashMap<>();
+		mp.put("name", "morpheus");
+		mp.put("job", "leader");
 		
 		given()
-		
+		  .contentType("application/json")
+		  .body(mp)
+		  
 		.when()
-		 .get("https://reqres.in/api/users?page=2")
-		
+		  .post("https://reqres.in/api/users")   
+
 		.then()
-		.statusCode(200)
-		.body("page", equalTo(2))
-		.log().all();
+		  .statusCode(201)
+		  .log().all();
 		
 	}
+	
 
 }
