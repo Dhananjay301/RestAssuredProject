12c935213a89f32b6df75ca40d576c9c56af7702
Dhananjay301 <dhananjaypatil301@gmail.com>
2025-04-06 15:01:33 +0530

    create , get and update user methods

--- src/test/java/day1/HttpsMethos.java
+++ src/test/java/day1/HttpsMethos.java
@@ -9,38 +9,61 @@ import static org.hamcrest.Matchers.*;
 import java.util.HashMap;
 
 public class HttpsMethos {
-
-//	@Test
-//	void getUser() {
-//
-//		     given()
-//
-//			.when()
-//			   .get("https://reqres.in/api/users?page=2")
-//
-//			.then()
-//			   .statusCode(200)
-//			   .body("page", equalTo(2))
-//			   .log().all();
-//
-//	}
 	
-	@Test
+	int id;
+
+	@Test(priority=1)
+	void getUser() {
+
+		     given()
+
+			.when()
+			   .get("https://reqres.in/api/users?page=2")
+
+			.then()
+			   .statusCode(200)
+			   .body("page", equalTo(2))
+			   .log().all();
+
+	}
+	
+	@Test(priority=2)
 	void createUser() {
 		HashMap mp=new HashMap<>();
 		mp.put("name", "morpheus");
 		mp.put("job", "leader");
 		
-		given()
+		id=given()
 		  .contentType("application/json")
 		  .body(mp)
 		  
-		.when()
-		  .post("https://reqres.in/api/users")   
+		 .when()
+		   .post("https://reqres.in/api/users")
+		   .jsonPath().getInt("id");
 
-		.then()
-		  .statusCode(201)
-		  .log().all();
+//		.then()
+//		  .statusCode(201)
+//		  .log().all();
+//		
+	}
+	
+	@Test(priority=3, dependsOnMethods= {"createUser"})
+	void updateUser()  {
+		HashMap data=new HashMap<>();
+		data.put("name", "Dhananjay");
+		data.put("job", "QA");
+		
+		given()
+		  .contentType("application/json")
+		  .body(data)
+		  
+		 .when()
+		   .post("https://reqres.in/api/users/"+id)
+		   
+		 .then()
+		   .statusCode(201)
+		   .log().all()
+		   .log().body();
 		
 	}
 	
